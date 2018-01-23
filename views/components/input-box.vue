3d97d86dbd135799ef498a5d795fdee5b3597331
Shigeki Suwa <suwa.shigeki@gmail.com>
2018-01-23 11:08:59 +0900

    implemented prototype of first page

---
+++ views/components/input-box.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+input(:type="type" :placeholder="placeholder")
+</template>
+
+<script>
+export default {
+  props: {
+    type: { type: String, default: "text" },
+    placeholder: { type: String, default: "" },
+  },
+}
+</script>
+
+<style lang="stylus">
+</style>
--- views/components/vue-app-root.vue
+++ views/components/vue-app-root.vue
@@ -1,15 +1,43 @@
 <template lang="pug">
 .vue-app-root
-  h1 HELLO
+  h1.title The Application
+  hr
+  form.form
+    div
+      label ID
+        input-box._ml-10(type="number" placeholder="00000")
+    div
+      submit-button
 </template>
 
 <script>
+import InputBox from './input-box.vue';
+import SubmitButton from './submit-button.vue';
+
 export default {
+  components: {
+    InputBox,
+    SubmitButton,
+  },
 }
 </script>
 
 <style lang="stylus">
+@require "../variables.styl";
+
 .vue-app-root {
-  color: red;
+  color: $black;
+
+  > .title {
+    padding: 0.5rem 1rem;
+  }
+
+  > .form {
+    padding: 1rem;
+
+    > * {
+      margin-bottom: 1rem;
+    }
+  }
 }
 </style>
