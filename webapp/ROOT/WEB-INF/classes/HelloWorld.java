465301744a08242686eb857d0d1184cdb1e87f19
Junya Katagai <al14028@gmail.com>
2018-01-23 13:09:44 +0900

    aaa

--- webapp/ROOT/WEB-INF/classes/HelloWorld.java
+++ webapp/ROOT/WEB-INF/classes/HelloWorld.java
@@ -11,14 +11,24 @@ public class HelloWorld extends HttpServlet{
     String id = request.getParameter("id");
     ResultInfo ri = new ResultInfo(id);
     ri.predict();
-    request.setAttribute("result",ri);
+
+    PrintWriter out = response.getWriter();
+    out.println("<user>");
+    out.println("<id>"+ri.getId()+"</id>");
+    out.println("<name>" + ri.getName()+"</name>");
+    out.println("<addr>"+ri.getAddress() + "</addr>");
+    out.println("<yesno>"+ri.getYesno() + "</yesno>");
+    out.println("</user>");
+    out.flush();
+    //request.setAttribute("out",out);
+    /*request.setAttribute("result",ri);
     request.setAttribute("id",ri.getId());
     request.setAttribute("name",ri.getName());
     request.setAttribute("address",ri.getAddress());
     request.setAttribute("yesno",ri.getYesno());
-
-    RequestDispatcher dispatch = request.getRequestDispatcher("/jsp/result.jsp");
-    dispatch.forward(request,response);
+*/
+ //   RequestDispatcher dispatch = request.getRequestDispatcher("/jsp/test.jsp");
+   // dispatch.forward(request,response);
   }
 }
 
