e8b8a942d23561f403da7c52fef272b107a72389
Shigeki Suwa <suwa.shigeki@gmail.com>
2018-01-23 12:10:18 +0900

    implemented testing for Ajax

--- views/components/input-box.vue
+++ views/components/input-box.vue
@@ -1,12 +1,22 @@
 <template lang="pug">
-input(:type="type" :placeholder="placeholder")
+input(
+  :value="value"
+  @input="input($event.target.value)"
+  :type="type"
+  :placeholder="placeholder")
 </template>
 
 <script>
 export default {
   props: {
+    value: { default: null },
     type: { type: String, default: "text" },
     placeholder: { type: String, default: "" },
+  },
+  methods: {
+    input(value) {
+      this.$emit('input', value);
+    },
   },
 }
 </script>
