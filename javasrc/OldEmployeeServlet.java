2e3c78de73e9618e806fca958861f172168ee5b2
Junya Katagai <al14028@gmail.com>
2018-01-26 17:00:21 +0900

    fix currentday in employee

--- javasrc/OldEmployeeServlet.java
+++ javasrc/OldEmployeeServlet.java
@@ -8,10 +8,13 @@ import java.util.List;
 
 public class OldEmployeeServlet extends HttpServlet{
   public void doGet(HttpServletRequest request, HttpServletResponse response){
+    String str = "select * from employee ";
+      String condition1 = "not exists(select * from retirement where employee.id = retirement.id) ";
+      String condition2 = "((YEAR(CURDATE()) - YEAR(employee.birthday)) - (RIGHT(CURDATE(), 5) < RIGHT(employee.birthday, 5)) ) >= 30 ";//over30
     try{
       request.setCharacterEncoding("UTF-8");
       response.setContentType("text/html; charset=UTF-8");
-      String sql = "select * from employee where not exists(select * from retirement where employee.id = retirement.id) and '1988-01-29' >= birthday";
+       String sql = str+ "where "+condition1 + "and " + condition2;
       request.setAttribute("sql",sql);
       RequestDispatcher dispatch = request.getRequestDispatcher("/EmployeeServlet");
       dispatch.forward(request,response);
--- javasrc/YoungEmployeeServlet.java
+++ javasrc/YoungEmployeeServlet.java
@@ -10,9 +10,13 @@ public class YoungEmployeeServlet extends HttpServlet{
   public void doGet(HttpServletRequest request, HttpServletResponse response){
 
     try{
+      String str = "select * from employee ";
+      String condition1 = "not exists(select * from retirement where employee.id = retirement.id) ";
+      String condition2 = "((YEAR(CURDATE()) - YEAR(employee.birthday)) - (RIGHT(CURDATE(), 5) < RIGHT(employee.birthday, 5)) ) < 20 ";//under20
       request.setCharacterEncoding("UTF-8");
       response.setContentType("text/html; charset=UTF-8");
-      String sql = "select * from employee where not exists(select * from retirement where employee.id = retirement.id) AND '1998-01-29' < birthday";
+      String sql = str+ "where "+condition1 + "and " + condition2;
+      //String sql = "select * from employee where not exists(select * from retirement where employee.id = retirement.id) AND (YEAR(CURDATE()) - YEAR(employee.birthday)) - (RIGHT(CURDATE(), 5) < RIGHT(employee.birthday, 5)) < employee.birthday";
       request.setAttribute("sql",sql);
       RequestDispatcher dispatch = request.getRequestDispatcher("/EmployeeServlet");
       dispatch.forward(request,response);
